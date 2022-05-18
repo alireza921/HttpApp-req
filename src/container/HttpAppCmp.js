f0c7e740c53dd22d07bdd73938c2a03d1cf711c1
AliReza <alirezaesm921@gmail.com>
2022-05-18 13:36:08 +0430

    manage Err on server Side

--- src/container/HttpAppCmp.js
+++ src/container/HttpAppCmp.js
@@ -8,28 +8,28 @@ import AddComment from "./addNewComment/AddNewComment";
 const HttpApp = () => {
   const [comment, setComment] = useState(null);
   const [commentId, setCommentId] = useState(null);
-
+  const [err, setErr] = useState(false);
   useEffect(() => {
     axios
       .get("http://localhost:3001/comments")
       .then((res) => {
         setComment(res.data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => setErr(true));
   }, []);
 
   const selectId = (id) => {
     setCommentId(id);
   };
 
-  const newCommentHandler = (newComment,setNewComment) => {
+  const newCommentHandler = (newComment, setNewComment) => {
     axios
       .post("http://localhost:3001/comments", {
         ...newComment,
         postId: 10,
       })
       .then((res) => axios.get("http://localhost:3001/comments"))
-      .then(res => setComment(res.data))
+      .then((res) => setComment(res.data))
       .catch();
     setNewComment({
       name: "",
@@ -39,22 +39,27 @@ const HttpApp = () => {
   };
   // console.log(commentId);
 
+  const renderComments = () => {
+    let renderCm = <p> the data is Loading . . . </p>;
+
+    if (err) renderCm = <p> data fetching field . . . </p>;
+
+    if (comment) {
+      renderCm = comment.map((c) => (
+        <Comment
+          key={c.id}
+          name={c.name}
+          email={c.email}
+          onClick={() => selectId(c.id)}
+        />
+      ));
+    }
+    return renderCm;
+  };
+
   return (
     <main className={styles.main}>
-      <section className={styles.commentHolder}>
-        {comment ? (
-          comment.map((c) => (
-            <Comment
-              key={c.id}
-              name={c.name}
-              email={c.email}
-              onClick={() => selectId(c.id)}
-            />
-          ))
-        ) : (
-          <p> loading . . . </p>
-        )}
-      </section>
+      <section className={styles.commentHolder}>{renderComments()}</section>
 
       <section className={styles.fullCommentHolder}>
         <FullComment commentId={commentId} setComment={setComment} />

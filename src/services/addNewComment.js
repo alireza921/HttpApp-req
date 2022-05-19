336ca1a100990432c8a5ad678fc439f1614c0ef9
AliReza <alirezaesm921@gmail.com>
2022-05-19 19:06:37 +0430

    interceptor Add-Set headers

--- src/services/addNewComment.js
+++ src/services/addNewComment.js
@@ -1,4 +1,15 @@
 import http from "./httpService";
-export function addNewComment(data) { 
-  return  http.post("/comments",data)
+export function addNewComment(data) {
+
+// const username = ''
+// const password = ''
+// const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
+
+  const token = "AliReza_ESM";
+  const header = {
+    headers: {
+      Authorization: `Basic  ${token}`,
+    },
+  };
+  return http.post("/comments", data, header);
 }
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,13 +1,34 @@
 import axios from "axios";
 
-axios.defaults.baseURL = ("http://localhost:3002");
+axios.defaults.baseURL = "http://localhost:3002";
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (err) => {
+    console.log(err);
+    return Promise.reject();
+  }
+);
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (err) => {
+    console.log(err);
+    return Promise.reject();
+  }
+);
 
 const http = {
   get: axios.get,
   post: axios.post,
   delete: axios.delete,
-  
+
   put: axios.put,
 };
 
-export default http ;
+export default http;
